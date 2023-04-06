424b2138d818a8784abea65997f1133ca222d431
basharbakkar <basharinitial.bakkar@gmail.com>
2023-04-06 18:04:34 +0300

    connect frontend and server

--- client/script.js
+++ client/script.js
@@ -18,32 +18,29 @@ function loader(element) {
   }, 300);
 }
 
-
 function typeText(element, text) {
-    let index = 0;
-  
-    let interval = setInterval(() => {
-      if (index < text.length) {
-        element.innerHTML += text.charAt(index);
-        index++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 20);
-  }
+  let index = 0;
 
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
 
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
 
-  function generateUniqueId() {
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
-  
-    return `id-${timestamp}-${hexadecimalString}`;
-  }
-  
-  function chatStripe(isAi, value, uniqueId) {
-    return `
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
           <div class="wrapper ${isAi && "ai"}">
               <div class="chat">
                   <div class="profile">
@@ -56,37 +53,54 @@ function typeText(element, text) {
               </div>
           </div>
       `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  // to clear the textarea input
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  // to focus scroll to the bottom
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div
+  const messageDiv = document.getElementById(uniqueId);
+
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv);
+
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+    console.log(err);
   }
-  
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  
-    const data = new FormData(form);
-  
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
-  
-    // to clear the textarea input
-    form.reset();
-  
-    // bot's chatstripe
-    const uniqueId = generateUniqueId();
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
-  
-    // to focus scroll to the bottom
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-  
-    // specific message div
-    const messageDiv = document.getElementById(uniqueId);
-  
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv);
-  
-   
-  
- 
-  };
-  
-
-
+};
